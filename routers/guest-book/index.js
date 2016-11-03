40c243076286d3b08e9626fad8e45b2f72fd9ea0
mrdulin <novaline@qq.com>
2016-11-03 15:59:35 +0800

    guest-book分支合并到master重构完毕

---
+++ routers/guest-book/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var logger = require('morgan');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var viewPath = './guest-book/';
+
+router.use(
+    logger('dev'),
+    bodyParser.urlencoded({extended: false}),
+    bodyParser.json()
+);
+
+router
+    .get('/', function(req, res) {
+        res.render(view + 'index');
+    })
+
+router
+    .route('/new-entry')
+    .get(function(req, res) {
+        res.render(view + 'new-entry');
+    })
+    .post(function(req, res) {
+        var entries = req.app.entries;
+
+        if(!req.body.title || !req.body.content) {
+            res.status(400).send('Entry must have a title and a body !');
+            return;
+        }   
+        entries.push({
+            title: req.body.title,
+            content: req.body.content,
+            published: new Date()
+        });
+
+        res.redirect('/');
+    })
+
+module.exports = router;
